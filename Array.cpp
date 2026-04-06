f9917f4344c0a94ed5c7019ec9a96a2f8ce6c89a
scrollshadow <zaldhypramana@gmail.com>
2026-04-06 15:21:31 +0700

    lupa

--- Array.cpp
+++ Array.cpp
@@ -9,6 +9,22 @@ int main(){
    int kamu[5];
 //data pa
    cout << "Data pada index 1 = " << aku[1] << endl;
-   
+//ganti index ke 1
+   aku[1] = 200;
+   cout << "Data pada index 1 = " << aku[1] << endl;
+   cout << "Data index 1 = " << aku[1] << endl;
+   cout << "Data index 0 = ";
+   cin >> kamu[0];
+   cout << "Data index 1 = ";
+   cin >> kamu[1];
+   cout << "Data index 2 = ";
+   cin >> kamu[2];
+   cout << "Data index 3 = ";
+   cin >> kamu[3];
+   cout << "Data index 4 = ";
+   cin >> kamu[4];
+   cout << endl;
+       
+
 
 }
