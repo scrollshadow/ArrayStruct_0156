52be3d76a3d58ab1ec4d417245bd854e2ad55625
scrollshadow <zaldhypramana@gmail.com>
2026-04-06 15:56:48 +0700

    1

---
+++ struct3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs;
+    cout << "Nama : " ;
+    getline(cin, mhs.nama);
+     cout << "ALamat Desa : " ;
+    getline(cin, mhs.alamat.desa);
+    cout << "ALamat Kota : " ;
+    cin >> mhs.alamat.kota;
+     cout << "Umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+    //tampilkan 
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Umur: " << mhs.umur << endl;
+}
