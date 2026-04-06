27c9916fb878be35c282517a1d41f56f09146a86
scrollshadow <zaldhypramana@gmail.com>
2026-04-06 15:44:11 +0700

    arraystruct

---
+++ struct.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs;
+    cout << "Nama : " ;
+    cin >> mhs.nama;
+     cout << "ALamat : " ;
+    cin >> mhs.alamat;
+     cout << "Umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+    //tampilakan 
+}
